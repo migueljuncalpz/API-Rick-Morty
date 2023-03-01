ee69c99a3b714c0edd5eb3c97b1f9d2a7f7664da
migueljuncalpz <miguelj@empathy.co>
2023-03-01 16:33:31 +0100

    Final corrections

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 addEventListener('DOMContentLoaded', () => {
-
-    ///declaración de atributos a usar
     const character_container = document.getElementById('character_container');
     const searchForm = document.querySelector('#search-form');
     const buttons = document.querySelectorAll('.filterButton');
@@ -10,11 +8,8 @@ addEventListener('DOMContentLoaded', () => {
         Alive: { color: '#16be00', fontcolor:'#000000'},
         unknown: { color: '#e3fc00', fontcolor:'#000000'},
     };
-
     let currentPage=1
     let url="https://rickandmortyapi.com/api/character/?"
-
-
     searchForm.addEventListener('submit', async function(event) {
         event.preventDefault();
         const query = this.elements.query.value;
@@ -24,11 +19,9 @@ addEventListener('DOMContentLoaded', () => {
         refreshPage(data.results)
         renderPagination(data.info.pages)
     });
-
     buttons.forEach( button =>{
         button.addEventListener("click",filterResults)
     })
-
     function filterResults(event){
         if(event.target.name==="none"){
             fetch(url)
@@ -47,14 +40,11 @@ addEventListener('DOMContentLoaded', () => {
                     renderPagination(data.info.pages)
                 })
         }
-
     }
-
     async function getCharacters() {
         const response = await fetch('https://rickandmortyapi.com/api/character');
         return response.json();
     }
-
     function createCharacterCard(character){
         const character_element = document.createElement('div');
         character_element.classList.add('character-card');
@@ -81,14 +71,13 @@ addEventListener('DOMContentLoaded', () => {
     }
     function limpiarHTML(html){
         html.innerHTML='';
-    };
+    }
     function refreshPage(character_list){
         limpiarHTML(character_container)
         for (let i=0 ; i<character_list.length ; i++){
             createCharacterCard(character_list[i])
         }
     }
-
     function renderPagination(totalPages) {
         // Crear botones de paginación
         const pagination = document.getElementById("pagination");
@@ -106,7 +95,6 @@ addEventListener('DOMContentLoaded', () => {
             pagination.appendChild(button);
         }
     }
-
     getCharacters().then( data => {
         const character_list = data.results
         for (let i=0 ; i<character_list.length ; i++){
@@ -114,5 +102,4 @@ addEventListener('DOMContentLoaded', () => {
         }
         renderPagination(data.info.pages)
     })
-
 })
